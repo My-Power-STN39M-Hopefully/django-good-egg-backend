29bfba985aa22d4e8a239b9c932970d7eed6cc3f
MorganMac1993 <sitesbymo@gmail.com>
2020-09-05 11:20:29 -0700

    added simple views, and updated models

--- good_egg/models.py
+++ good_egg/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Incident(models.Model):
+  YES = 'y'
+  NO = 'n'
+  yes_or_no = (
+    (YES, 'yes'),
+    (NO, 'no'),
+  )
   category = models.CharField(max_length=100)
   officers =  models.ForeignKey(Officer, on_delete=models.CASCADE, related_name='officer')
   date = models.CharField(max_length=100)
   time = models.CharField(max_length=100)
   location = models.CharField(max_length=100)
   formal_complaint = models.CharField(max_length=100)
-  formal_complaint_number = models.CharField(max_length=100)
-  witnesses_present = models.CharField(max_length=100)
+  formal_complaint_number = models.CharField(max_length=100, choices=yes_or_no, default=NO)
+  witnesses_present = models.CharField(max_length=100, choices=yes_or_no, default=NO)
   witnesses_information = models.CharField(max_length=100)
   User_id = models.CharField(max_length=100)
-  private = models.CharField(max_length=100)
+  private = models.CharField(max_length=100, choices=yes_or_no, default=NO)
   bad_apple = models.CharField(max_length=100, default = True)
 
   def __str__(self):
@@ -24,8 +30,8 @@ class Officer(models.Model):
   last_name = models.CharField(max_length=100)
   dob = models.CharField(max_length=100)
   badge_number = models.CharField(max_length=100)
-  nationality = models.CharField(max_length=100)
-  race = models.CharField(max_length=100)
+  nationality = models.CharField(max_length=100, default='N/A')
+  race = models.CharField(max_length=100, default='N/A')
   gender = models.CharField(max_length=100)
   force = models.CharField(max_length=100)
   active = models.CharField(max_length=100)
--- good_egg/views.py
+++ good_egg/views.py
@@ -1,5 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Incident
+from .models import Officer
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+def incident_list(req):
+  incidents = Incident.objects.all()
+  return render(req, 'good_egg/incident_list.html', {'incidents': incidents}) 
+
+def officer_list(req):
+  officers = Officer.objects.all()
+  return render(req, 'good_egg/officer_list.html', {'officers': officers})
+
+def incident_detail(req, pk):
+  incident = Incident.objects.get(id=pk)
+  return render(req, 'good_egg/incident_detail.html', {'incident': incident})
+
+def officer_detail(req, pk):
+  officer = Officer.objects.get(id=pk)
+  return render(req, 'good_egg/officer_detail.html', {'officer': officer})
+
+@login_required
+def incident_create(req):
+    if req.method == 'POST':
+        form = IncidentForm(req.POST)
+        if form.is_valid():
+            incident = form.save()
+            return redirect('incident_detail', pk=incident.pk)
+    else:
+        form = IncidentForm()
+    return render(req, 'good_egg/incident_form.html', {'form': form})
+
+# Admin Auth
+# @login_required  
+# def officer_create(req):
+#     if req.method == 'POST':
+#         form = officerForm(req.POST)
+#         if form.is_valid():
+#             officer = form.save()
+#             return redirect('officer_detail', pk=officer.pk)
+#     else:
+#         form = officerForm()
+#     return render(req, 'good_egg/officer_form.html', {'form': form})
+
+@login_required  
+def incident_edit(req, pk):
+    incident = Incident.objects.get(pk=pk)
+    if req.method == 'POST':
+      form = IncidentForm(req.POST, instance=incident)
+      if form.is_valid():
+        incident = form.save()
+        return redirect('incident_detail', pk=incident.pk)
+    else:
+        form = IncidentForm(instance=incident)
+    return render(req, 'good_egg/incident_form.html', {'form': form})
+  
+#  Admin Auth 
+# @login_required  
+# def officer_edit(req, pk):
+#     officer = officer.objects.get(pk=pk)
+#     if req.method == "POST":
+#       form = officerForm(req.POST, instance=officer)
+#       if form.is_valid():
+#         officer = form.save()
+#         return redirect('officer_detail', pk=officer.pk)
+#     else:
+#         form = officerForm(instance=officer)
+#     return render(req, 'good_egg/officer_form.html', {'form': form})
+  
+@login_required  
+def incident_delete(req, pk):
+  Incident.objects.get(id=pk).delete()
+  return redirect('incident_list')
+
+# Admin auth
+# @login_required
+# def officer_delete(req, pk):
+#   officer.objects.get(id=pk).delete()
+#   return redirect('officer_list')
